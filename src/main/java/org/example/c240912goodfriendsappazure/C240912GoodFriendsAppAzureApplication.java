99c226f26a3758dcbe32fc12b909b10fe007a089
Christoffer <chri26eq@stud.kea.dk>
2024-09-12 12:50:33 +0200

    init

---
+++ src/main/java/org/example/c240912goodfriendsappazure/C240912GoodFriendsAppAzureApplication.java
@@ -0,0 +1,13 @@
+package org.example.c240912goodfriendsappazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C240912GoodFriendsAppAzureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C240912GoodFriendsAppAzureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/c240912goodfriendsappazure/C240912GoodFriendsAppAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.c240912goodfriendsappazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C240912GoodFriendsAppAzureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
